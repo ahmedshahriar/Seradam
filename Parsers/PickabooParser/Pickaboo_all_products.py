2810f88c5d0c1591231474cd35084c8aa2602bba
Jishnu Saha <jishnusaha89@gmail.com>
2019-07-07 18:10:27 +0600

    pickaboo parsing: mobile phone parsing updated and laptop parsing done

--- Parsers/PickabooParser/Pickaboo_all_products.py
+++ Parsers/PickabooParser/Pickaboo_all_products.py
@@ -1,15 +1,32 @@
 
 from functions import *
 
+
+#get Smart Phones Categories
 smartphone_categories = get_smartphone_categories()
 
+#get All Smart Phones Lists
+for smartphone_categoriy in smartphone_categories:
+    get_smartphone_list(smartphone_categoriy)
+
+
+
+#get Feature Phones Categories
 featurephone_categories = get_featurephone_categories()
 
+#get All Feature Phones Lists
 for featurephone_categoriy in featurephone_categories:
     get_featurephone_list(featurephone_categoriy)
 
-for smartphone_categoriy in smartphone_categories:
-    get_smartphone_list(smartphone_categoriy)
+
+
+
+#get Laptop Categories
+loptop_categories = get_laptop_categories()
+
+#get All Laptop lists
+for category in loptop_categories:
+    get_laptop_list(category)
 
 
 print('done')
--- Parsers/PickabooParser/functions.py
+++ Parsers/PickabooParser/functions.py
@@ -6,12 +6,49 @@ def get_smartphone_categories():
     html = urlopen("https://www.pickaboo.com/mobile-phone/smartphone.html")
     data = html.read()
     soup = BeautifulSoup(data, 'html.parser')
-    categories = soup.findAll('div' , { "class": "submain-categ"} )
+    categories = soup.findAll('div', {"class": "submain-categ"} )
     smartphone_categories = []
     for category in categories:
-        smartphone_categories.append(category.a['title'].lower())
+        link = category.a['href'];
+        smartphone_categories.append(link.split('/')[-1].split('.')[-2])
 
-    return  smartphone_categories
+    return smartphone_categories
+
+
+
+
+def get_smartphone_list(category):
+    url = "https://www.pickaboo.com/mobile-phone/smartphone/" + category + ".html"
+
+    json_file = "./json/mobile/SmartPhone_" + category + "_list.json"
+    write_file = open(json_file, 'w')
+
+    total_products = []
+    while url:
+        html = urlopen(url)
+        data = html.read()
+        soup = BeautifulSoup(data, 'html.parser')
+        products = soup.find_all('h2', {'class': 'product-name newname'});
+        for product in products:
+            product_url = product.a['href']
+            title = product.a.string
+            product_records = {
+                "product_title": title,
+                "product_link": product_url
+            }
+            total_products.append(product_records)
+
+        url = soup.find('a', {'class': 'next i-next', 'title': 'Next'})
+
+        if url:
+            url = url.get('href')
+        else:
+            break
+    json.dump({"Products": total_products}, write_file, sort_keys=True, indent=4, separators=(',', ': '))
+    write_file.close()
+
+
+
 
 
 
@@ -19,54 +56,21 @@ def get_featurephone_categories():
     html = urlopen("https://www.pickaboo.com/mobile-phone/feature-phone.html")
     data = html.read()
     soup = BeautifulSoup(data, 'html.parser')
-    categories = soup.findAll('p' , { "class": "main-parent-category"} )
+    categories = soup.findAll('p', {"class": "main-parent-category"})
     featurephone_categories = []
     for category in categories:
-        featurephone_categories.append(category.a['title'].lower())
+        link = category.a['href'];
+        featurephone_categories.append(link.split('/')[-1].split('.')[-2])
 
-    return  featurephone_categories
+    return featurephone_categories
 
-
-
-def get_smartphone_list(category):
-    url = "https://www.pickaboo.com/mobile-phone/smartphone/" + category + ".html"
-
-    json_file = "SmartPhone_" + category + "_list.json"
-    write_file = open(json_file, 'w')
-
-    total_products = []
-    while url:
-        html = urlopen(url)
-        data = html.read()
-        soup = BeautifulSoup(data, 'html.parser')
-        #print("url : " + url)
-        products = soup.find_all('h2', {'class': 'product-name newname'});
-        for product in products:
-            product_url = product.a['href']
-            title = product.a.string
-            #print(title)
-            product_records = {
-                "product_title": title,
-                "product_link": product_url
-            }
-            total_products.append(product_records)
-        #print("loop ses")
-        url = soup.find('a', {'class': 'next i-next', 'title': 'Next'})
-        #print("url : ", end="")
-        #print(url)
-        if url:
-            url = url.get('href')
-        else:
-            break
-    json.dump({"Products": total_products}, write_file, sort_keys=True, indent=4, separators=(',', ': '))
-    write_file.close()
 
 
 
 def get_featurephone_list(category):
     url = "https://www.pickaboo.com/mobile-phone/feature-phone/" + category + ".html"
 
-    json_file = "FeaturePhone_"+category+"_list.json"
+    json_file = "./json/mobile/FeaturePhone_"+category+"_list.json"
     write_file = open(json_file, 'w')
 
     total_products = []
@@ -74,24 +78,74 @@ def get_featurephone_list(category):
         html = urlopen(url)
         data = html.read()
         soup = BeautifulSoup(data, 'html.parser')
-        #print("url : "+ url)
         products = soup.find_all('h2', {'class': 'product-name newname'});
         for product in products:
             product_url = product.a['href']
             title = product.a.string
-            #print(title)
             product_records = {
                 "product_title": title,
                "product_link": product_url
             }
             total_products.append(product_records)
-        #print("loop ses")
+
         url = soup.find('a', {'class': 'next i-next', 'title': 'Next'})
-        #print("url : ",end="")
-        #print(url)
+
         if url:
             url = url.get('href')
         else:
             break
     json.dump({"Products": total_products}, write_file, sort_keys=True, indent=4, separators=(',', ': '))
     write_file.close()
+
+
+
+
+def get_laptop_categories():
+    html = urlopen("https://www.pickaboo.com/computer-pc/laptop-notebook.html/")
+    data = html.read()
+    soup = BeautifulSoup(data, 'html.parser')
+    categories = soup.findAll('p', {"class": "main-parent-category"})
+    laptop_categories = []
+    for category in categories:
+        link = category.a['href'];
+        laptop_categories.append(link.split('/')[-1].split('.')[-2])
+
+    return laptop_categories
+
+
+
+
+
+def get_laptop_list(category):
+    url = "https://www.pickaboo.com/computer-pc/laptop-notebook/" + category + ".html"
+
+    json_file = "./json/laptop/Laptop_"+category+"_list.json"
+    write_file = open(json_file, 'w')
+
+    total_products = []
+    while url:
+        html = urlopen(url)
+        data = html.read()
+        soup = BeautifulSoup(data, 'html.parser')
+        products = soup.find_all('h2', {'class': 'product-name newname'});
+        for product in products:
+            product_url = product.a['href']
+            title = product.a.string
+
+            product_records = {
+                "product_title": title,
+               "product_link": product_url
+            }
+            total_products.append(product_records)
+        url = soup.find('a', {'class': 'next i-next', 'title': 'Next'})
+
+        if url:
+            url = url.get('href')
+        else:
+            break
+    json.dump({"Products": total_products}, write_file, sort_keys=True, indent=4, separators=(',', ': '))
+    write_file.close()
+
+
+
+
